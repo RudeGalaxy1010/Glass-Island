7571ec71639ac164d4f7f4fdd34a5450773e122a
RudeGalaxy1010 <Karev007@bk.ru>
2022-07-30 00:27:33 +0300

    tiles dissolve

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,13 +7,20 @@ namespace GlassIsland
     {
         [SerializeField] private Vector3 _shift;
         [SerializeField] private float _speed;
+        [SerializeField] private float _extinctTime;
+        [SerializeField] private Collider _tileColldier;
 
         private Vector3 _idlePosition;
         private Vector3 _pressedPosition;
         private Vector3 _targetPosition;
 
+        private Material _tileMaterial;
+        private bool _isPressed;
+        private float _timer;
+
         private void Start()
         {
+            _tileMaterial = _tileColldier.GetComponent<Renderer>().material;
             _idlePosition = transform.position;
             _pressedPosition = _idlePosition + _shift;
             _targetPosition = _idlePosition;
@@ -23,7 +30,13 @@ namespace GlassIsland
         {
             if (other.TryGetComponent(out Player player))
             {
+                if (_isPressed == true)
+                {
+                    return;
+                }
+
                 _targetPosition = _pressedPosition;
+                StartDissolving();
             }
         }
 
@@ -37,10 +50,40 @@ namespace GlassIsland
 
         private void Update()
         {
+            if (_isPressed == true)
+            {
+                Dissolve();
+            }
+
+            Move();
+        }
+
+        private void Move()
+        {
             if (transform.position != _targetPosition)
             {
                 transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
             }
+        }
+
+        private void Dissolve()
+        {
+            _timer += Time.deltaTime;
+
+            float materialAlpha = 1 - (_timer / _extinctTime);
+            _tileMaterial.color = new Color(_tileMaterial.color.r, _tileMaterial.color.g, _tileMaterial.color.b, materialAlpha);
+
+            if (_timer >= _extinctTime)
+            {
+                _tileColldier.gameObject.SetActive(false);
+                _isPressed = false;
+            }
+        }
+
+        private void StartDissolving()
+        {
+            _isPressed = true;
+            _timer = 0;
         }
     }
 }
