f78d878526911957232aab1858ca0dd2a1f93e2f
RudeGalaxy1010 <Karev007@bk.ru>
2022-08-01 16:25:36 +0300

    Player animator improments

---
+++ Assets/Scripts/Player/PlayerAnimatorConstants.cs
@@ -0,0 +1,4 @@
+public static class PlayerAnimatorConstants
+{
+    public const string RunningAnimation = "IsRunning";
+}
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -5,6 +5,9 @@ namespace GlassIsland
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerMove : MonoBehaviour
     {
+        private const float IdleAnimatorSpeed = 1;
+        private const float MaxDirectionVectorLength = 1.41f;
+
         [SerializeField] private Animator _animator;
         [SerializeField] private Joystick _joystick;
         [SerializeField] private float _speed;
@@ -28,11 +31,13 @@ namespace GlassIsland
             {
                 var moveDirection = new Vector3(_joystick.Direction.x, 0, _joystick.Direction.y);
                 _rigidbody.MovePosition(transform.position + moveDirection * _speed * Time.deltaTime);
-                _animator.SetBool("IsRunning", true);
+                _animator.SetBool(PlayerAnimatorConstants.RunningAnimation, true);
+                _animator.speed = moveDirection.magnitude / MaxDirectionVectorLength;
             }
             else
             {
-                _animator.SetBool("IsRunning", false);
+                _animator.SetBool(PlayerAnimatorConstants.RunningAnimation, false);
+                _animator.speed = IdleAnimatorSpeed;
             }
         }
 
